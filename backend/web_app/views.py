7d1d6c599d274060ae007ecf2653f3d2451b6b26
Sala Situacional <comandanciabomberossc2024@gmail.com>
2024-09-02 11:44:57 -0400

    reamplazando vite

--- backend/web_app/views.py
+++ backend/web_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def Home(request):
+    return HttpResponse("Hello World!")
+
+def Render(request):
+    render(request, "Cuerpo_bomberos\frontend\index.html")
