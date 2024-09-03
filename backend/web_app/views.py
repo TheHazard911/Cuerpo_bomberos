d86be2a6cb1ec39da4a7d80254d7d49620578720
Eber_C <ghack7756@gmail.com>
2024-09-03 00:47:40 -0400

    nuevos archivos

--- backend/web_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def Home(request):
-    return HttpResponse("Hello World!")
-
-def Render(request):
-    render(request, "Cuerpo_bomberos\frontend\index.html")
