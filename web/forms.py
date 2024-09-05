020cb616f4fb3e85e78c0bb5f470b9b865927e9e
Anthony Mora <crepystories1@gmail.com>
2024-09-05 19:08:04 -0400

    Agregando archivo forms.py y corrigiendo errores

---
+++ web/forms.py
@@ -0,0 +1,3 @@
+from django import forms
+
+# Creacion de Formularios que se podran mostrar en el sitio web.
--- web/models.py
+++ web/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Crear Modelos (Bases de datos) propios.
 class Usuarios(models.Model):
     user = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
